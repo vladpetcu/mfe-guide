47751f837cd48d22e2768e5248784465d81c8fb5
Vlad Petcu <vlad.petcu@mercell.com>
2021-04-23 14:43:02 +0300

    trigger redeploy for test

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  console.log("containerr");
+
   return (
     <div>
       <h1>Hi there</h1>
