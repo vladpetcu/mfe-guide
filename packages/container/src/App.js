674d1ad6b8720f55dbb584568975c653618921a3
Vlad Petcu <vlad.petcu@mercell.com>
2021-04-23 13:02:40 +0300

    try redeploy to s3

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("containerr");
-
   return (
     <div>
       <h1>Hi there</h1>
