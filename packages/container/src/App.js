d4f3cb53cb334fb2c1d5b117cdd0395b2ce064a5
Vlad Petcu <vlad.petcu@mercell.com>
2021-04-23 11:59:08 +0300

    commit to trigger actions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
+  console.log("container");
+
   return (
     <div>
       <h1>Hi there</h1>
