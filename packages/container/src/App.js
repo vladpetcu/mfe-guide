283effc7348c7901bfa0a784c92a795a5928f6c1
Vlad Petcu <vlad.petcu@mercell.com>
2021-04-23 12:45:31 +0300

    try redeploy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("container");
+  console.log("containerr");
 
   return (
     <div>
