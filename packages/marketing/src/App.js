49bc8c6ea08acf962045993f96853f0a06059243
Vlad Petcu <vlad.petcu@mercell.com>
2021-04-23 11:56:03 +0300

    removed console log

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,8 +6,6 @@ import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 
 export default () => {
-  console.log(";in marketing app");
-
   return (
     <div>
       <StylesProvider>
